d2f8b9481d67b6ab91250fc01e284be133955ef5
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-05-03 13:23:01 +0100

    fetching games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -4,6 +4,11 @@ const apiKey = import.meta.env.DEV
   ? import.meta.env.VITE_API_KEY
   : process.env.API_KEY;
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
