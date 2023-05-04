102880569a19fcc16ad444fc5e60151bc62b3616
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-05-04 14:18:24 +0100

    simplify calculation logic

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { GameQuery } from "./../App";
 import { Platform } from "./usePlatforms";
@@ -27,7 +28,7 @@ const useGames = (gameQuery: GameQuery) => {
           page: pageParam,
         },
       }),
-    staleTime: 1 * 60 * 60 * 1000,
+    staleTime: ms("24h"),
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
